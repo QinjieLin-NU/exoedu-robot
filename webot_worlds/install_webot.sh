1fda2675053e9fd122b741cae2981d706578ef57
qinjielin-nu <qinjielin2018@u.northwestern.edu>
2021-06-16 22:25:58 +0000

    add install

---
+++ webot_worlds/install_webot.sh
@@ -0,0 +1,5 @@
+wget https://github.com/cyberbotics/webots/releases/download/R2021a/webots-R2021a-x86-64.tar.bz2
+tar xvfj webots-R2021a-x86-64.tar.bz2
+sudo apt install python3-pip
+sudo -H pip3 install --upgrade pip
+sudo -H pip3 install websocket-client tornado nvidia-ml-py3 psutil requests distro
